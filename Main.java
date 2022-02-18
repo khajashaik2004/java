349c153691b3112f0f9d7fbaa6d0601c24b9c22b
khajashaik2004 <92737226+khajashaik2004@users.noreply.github.com>
2022-02-18 12:28:19 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,16 @@
+package com.apnacollage;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        System.out.println("hello world");
+
+        System.out.println("khaja shaik");
+
+        System.out.println("kanna");
+    }
+
+
+}
